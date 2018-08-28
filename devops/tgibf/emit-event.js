721d015326153f6b0f2efea48ad646dfa09dde1e
Evandro Saroka <evandro.saroka@teravoz.com.br>
2018-08-28 16:41:59 -0300

    adding more events

--- devops/tgibf/emit-event.js
+++ devops/tgibf/emit-event.js
@@ -3,34 +3,34 @@ const events = require('./events');
 
 // Sends increasing bursts of events
 module.exports = function startEvents(webhook, i=0) {
-  setTimeout(() => {
-    ++i;
-    let total = i*1000;
-    let sent = 0;
-    let errored = false;
-    console.log(`Gonna emit ${total} events`);
-    for (var j = 0; j < total; j++) {
-      emitEvent(webhook, (err) => {
-        if (err) {
-          errored = true;
+  ++i;
+  let total = i*1000;
+  let sent = 0;
+  let errored = false;
+  console.log(`Gonna emit ${total} events`);
+  for (var j = 0; j < total; j++) {
+    emitEvent(webhook, (err) => {
+      if (err) {
+        errored = true;
+      }
+      ++sent;
+      if (sent == total) {
+        console.info(`Emitted ${total} events`);
+        if (errored) {
+          console.error(`Got error on i = ${i}, setting i=i-2 to keep pressure`);
+          i = i-2;
         }
-        ++sent;
-        if (sent == total) {
-          console.info(`Emitted ${total} events`);
-          if (errored) {
-            console.error(`Got error on i = ${i}, setting i=i-2 to keep pressure`);
-            i = i-2;
-          }
+        setTimeout(() => {
           startEvents(webhook, i);
-        }
-      });
-    }
-  }, 2000);
+        }, 2000);
+      }
+    });
+  }
 }
 
 function emitEvent(webhook, callback) {
   // random event
-  let i = Math.floor(Math.random() * 4);
+  let i = Math.floor(Math.random() * 40);
 
   console.info(`Emitting event `, events[i]);
   request.post({
@@ -39,9 +39,9 @@ function emitEvent(webhook, callback) {
   })
   .on('response', (response) => {
     if (response.statusCode == 200) {
-      console.log(`INFO: POST status code response is ${response.statusCode} for event `, events[i]);
+      console.log(`INFO: POST status code response is ${response.statusCode} for events[i] = `, events);
     } else {
-      console.warn(`WARN: POST status code response is ${response.statusCode} for event `, events[i]);
+      console.warn(`WARN: POST status code response is ${response.statusCode} for events[i] = `, events);
     }
     callback(null);
   })
